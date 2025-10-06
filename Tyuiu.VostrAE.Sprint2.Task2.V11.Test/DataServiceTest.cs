b0e0cd796f9a8c2a4ac00b34a2d3dd125e511ab9
A Vostr <Tyuiu.VostrAE@outlook.com>
2025-10-06 22:52:15 +0500

    Task2

---
+++ Tyuiu.VostrAE.Sprint2.Task2.V11.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using System.Security.Cryptography.X509Certificates;
+using Tyuiu.VostrAE.Sprint2.Task2.V11.Lib;
+namespace Tyuiu.VostrAE.Sprint2.Task2.V11.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            int x = 9;
+            int y = 3;
+            bool res = ds.CheckDotInShadedArea(x, y);
+            bool wait = true;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.VostrAE.Sprint2.Task2.V11/Program.cs
@@ -0,0 +1,22 @@
+﻿using Tyuiu.VostrAE.Sprint2.Task2.V11.Lib;
+class Program
+{
+    public static void Main()
+    {
+        DataService ds = new DataService();
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine(" Исходные данные:                                                         *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("Введите число X:");
+        int X = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Введите число Y:");
+        int y = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Результат:                                                              *");
+        Console.WriteLine("***************************************************************************");
+        bool res = ds.CheckDotInShadedArea(X, y);
+        if (res) Console.WriteLine("Точка находится в заштрихованной области");
+        else Console.WriteLine("Точка не находится в заштрихованной области");
+        Console.ReadKey(); }
+}
