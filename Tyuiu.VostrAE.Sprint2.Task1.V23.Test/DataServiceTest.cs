9f2a87a22dae4a63d27a20f3677c32c0e5e70207
A Vostr <Tyuiu.VostrAE@outlook.com>
2025-10-06 19:32:17 +0500

    Task1

---
+++ Tyuiu.VostrAE.Sprint2.Task1.V23.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using Tyuiu.VostrAE.Sprint2.Task1.V23.Lib;
+
+namespace Tyuiu.VostrAE.Sprint2.Task1.V23.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            int a = 242; int b = 571; int c = 325; int d = 155;
+            bool[] res = ds.GetLogicOperations(a, b, c, d);
+            bool[] wait = { false, false, false, true, true, true }; 
+
+            CollectionAssert.AreEqual(wait, res);
+        }
+    }
+}
