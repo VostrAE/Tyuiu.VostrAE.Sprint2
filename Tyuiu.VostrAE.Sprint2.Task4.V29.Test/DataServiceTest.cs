dd259e176fb1e690ece240eff7ab0571ca9138c1
A Vostr <Tyuiu.VostrAE@outlook.com>
2025-10-07 15:36:39 +0500

    Task4

---
+++ Tyuiu.VostrAE.Sprint2.Task4.V29.Test/DataServiceTest.cs
@@ -0,0 +1,28 @@
+﻿using Tyuiu.VostrAE.Sprint2.Task4.V29.Lib;
+namespace Tyuiu.VostrAE.Sprint2.Task4.V29.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            double x = -100;
+            double y = 2;
+            double res = ds.Calculate(x, y);
+            double wait = -175.98;
+            Assert.AreEqual(res, wait);
+        }
+        [TestMethod]
+        public void TestMethod2()
+        {
+            DataService ds = new DataService();
+            double x = 100;
+            double y = 2;
+            double res = ds.Calculate(x, y);
+            double wait = 9999;
+            Assert.AreEqual(res, wait);
+        }
+    }
+}
---
+++ Tyuiu.VostrAE.Sprint2.Task4.V29/Program.cs
@@ -0,0 +1,21 @@
+﻿using Tyuiu.VostrAE.Sprint2.Task4.V29.Lib;
+class Program
+{
+    public static void Main()
+    {
+        DataService ds = new DataService();
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine(" Исходные данные:                                                         *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("Введите число X:");
+        int x = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Введите число Y:");
+        int y = Convert.ToInt32(Console.ReadLine());
+        double res = ds.Calculate(x,y);
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Результат:                                                              *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("Значение функции= " + res);
+        Console.ReadKey();
+    }
+}
